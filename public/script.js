04887e1943adc2038e93d907384a8d62290881f3
alexleo6 <en47489+393@gmail.com>
2024-08-11 21:53:43 -0400

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,10 @@
+document.getElementById('playButton').addEventListener('click', function() {
+    document.getElementById('message').style.animation = 'fadeOut 1s forwards';
+    setTimeout(() => {
+      window.location.href = 'https://66b96a44b4d640f6c724619a--recracked.netlify.app/';
+    }, 1000);
+  });
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('message').style.animation = 'fadeIn 1s forwards';
+  });
